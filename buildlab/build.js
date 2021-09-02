ef52343849ba525e77cbb4014617885c7cfac459
Jason Hargrove <jasonhargrove1@gmail.com>
2021-09-02 15:08:50 -0600

    lint

--- buildlab/build.js
+++ buildlab/build.js
@@ -1,8 +1,8 @@
 // npm run build (or npm run prebuild)
 
 import {
-	draw_contributions,
-	fetch_data_and_store
+  draw_contributions,
+  fetch_data_and_store
 } from './lab-a/lab_a.js';
 
 const execSync = require('child_process').execSync;
@@ -10,12 +10,12 @@ const execSync = require('child_process').execSync;
 console.log('argv', JSON.stringify(process.argv, null, 2));
 
 const make_github_image = async () => {
-	await fetch_data_and_store();
-	await draw_contributions();
+  await fetch_data_and_store();
+  await draw_contributions();
 };
 
 const copy_content = () => {
-	execSync(`cp README.md ./public`, { stdio:[0, 1, 2] });
+  execSync(`cp README.md ./public`, { stdio:[0, 1, 2] });
 };
 
 copy_content();
