eee216be8d1b8322f008f2308d8b416d1ab454c7
Jason Hargrove <jasonhargrove1@gmail.com>
2021-09-01 22:22:01 -0600

    lint

--- src/App.test.js
+++ src/App.test.js
@@ -2,5 +2,5 @@ import React from 'react';
 import { render } from '@testing-library/react';
 
 test('expect a placeholder to test true', () => {
-	expect(true).toBe(true);
+  expect(true).toBe(true);
 });
--- src/views/Home.js
+++ src/views/Home.js
@@ -5,43 +5,43 @@ import ReactMarkdown from 'react-markdown';
 import './Home.scss';
 
 function Home() {
-	const [readme_md, set_readme_md] = useState('');
+  const [readme_md, set_readme_md] = useState('');
 
-	const get_content = async () => {
-		const readme = await fetch('/README.md');
-		const text = await readme.text();
-		set_readme_md(text);
-	};
+  const get_content = async () => {
+    const readme = await fetch('/README.md');
+    const text = await readme.text();
+    set_readme_md(text);
+  };
 
-	get_content();
+  get_content();
 
   return (
     <div className="jh-home">
-    	<div className="jhh-container">
-	  		<figure>
-		      <img src="/jasonhargrove-jh-icon.png" alt="logo icon that says 'JH', an acronym for Jason Hargrove. the highlight color is orange" />
-		      <figcaption>
-		      	<h1>Jason Hargrove</h1>
-		      	<p>
-		      		<span>GitHub&nbsp;</span>
-		      		<span><a href="https://github.com/jasonhargrove">jasonhargrove</a></span>
-		      	</p>
-		      	<p>
-		      		<span>Twitter&nbsp;</span>
-		      		<span><a href="https://twitter.com/jasonhargrove">jasonhargrove</a></span>
-		      	</p>
-		      </figcaption>
-				</figure>
-	      <ReactMarkdown className='jhhc-content'>{readme_md}</ReactMarkdown>
-    	</div>
-    	<div className='jhh-footer'>
-    	  <img src="/site-header.jpg" alt="banner style graphic that says 'imagine'" />
-    	</div>
-    	<p>
-    	  <span>PS, this app is&nbsp;</span>
-	    	<span><a href="https://github.com/jasonhargrove/jasonhargrove">public on GitHub, see how it's made?</a></span>
-    	</p>
-    	<p>👋 JH</p>
+      <div className="jhh-container">
+        <figure>
+          <img src="/jasonhargrove-jh-icon.png" alt="logo icon that says 'JH', an acronym for Jason Hargrove. the highlight color is orange" />
+          <figcaption>
+            <h1>Jason Hargrove</h1>
+            <p>
+              <span>GitHub&nbsp;</span>
+              <span><a href="https://github.com/jasonhargrove">jasonhargrove</a></span>
+            </p>
+            <p>
+              <span>Twitter&nbsp;</span>
+              <span><a href="https://twitter.com/jasonhargrove">jasonhargrove</a></span>
+            </p>
+          </figcaption>
+        </figure>
+        <ReactMarkdown className='jhhc-content'>{readme_md}</ReactMarkdown>
+      </div>
+      <div className='jhh-footer'>
+        <img src="/site-header.jpg" alt="banner style graphic that says 'imagine'" />
+      </div>
+      <p>
+        <span>PS, this app is&nbsp;</span>
+        <span><a href="https://github.com/jasonhargrove/jasonhargrove">public on GitHub, see how it's made?</a></span>
+      </p>
+      <p>👋 JH</p>
     </div>
   );
 }
