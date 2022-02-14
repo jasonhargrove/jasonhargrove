8d31f0dc25c35d48034f57c1bca27e68e12ba5d1
Jason Hargrove <jasonhargrove1@gmail.com>
2022-02-14 03:21:07 -0700

    Photos carousel, responsive styling

--- src/features/Photos.js
+++ src/features/Photos.js
@@ -1,19 +1,27 @@
 import { useEffect, useState } from 'react';
+function randomBetween (min, max) {
+  return Math.floor(Math.random() * (max - min + 1) + min);
+};
+
 export function Photos() {
-	const max = 21;
-	const path = '/collection/op15b/{x}.jpeg';
-	const [count, setCount] = useState(0);
-	useEffect(() => {
-		setTimeout(x => {
-			setCount(count < max ? count + 1 : 0);
-		}, 2000)
-	}, [count]);
-	const src = path.replace(/{x}/, count);
+  const experimentalLandscape = [3,6,8,10,11,13,14,16,17,18,20,21];
+  const max = 21;
+  const posmax = 5;
+  const path = '/collection/op15b/{x}.jpeg';
+  const time = 2000;
+  const [count, setCount] = useState(0);  useEffect(() => {
+    setTimeout(x => {
+      setCount(count < max ? count + 1 : 0);
+    }, time);
+  }, [count]);
+  const src = path.replace(/{x}/, count);
+  const className = `jh-photos img-${count} img-pos-${randomBetween(0,posmax)}`
+                  + ` img-orientation-${experimentalLandscape.includes(count) ? 'l' : 'p'}`;
   return (
-  	<div className={`jh-photos img-${count}`}>
-  	  <a href={src} target="_blank">
-	  	  <img src={src} />
-  	  </a>
-  	</div>
+    <div className={className}>
+      <a href={src} target="_blank" rel="noreferrer">
+        <img src={src} alt='Photography by J.Hargrove' />
+      </a>
+    </div>
   );
 }
