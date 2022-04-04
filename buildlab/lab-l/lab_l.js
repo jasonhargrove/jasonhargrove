34d30f72bf6e1ad763f94d485c77d832f1943c1e
Jason Hargrove <jasonhargrove1@gmail.com>
2022-04-03 23:02:43 -0600

    fix tabs to spaces

--- buildlab/lab-l/lab_l.js
+++ buildlab/lab-l/lab_l.js
@@ -6,23 +6,23 @@ export async function process_log_for_microblog() {
   try {
     console.log(' process_log_for_microblog '.bgWhite.black);
 
-		const log = fs.readFileSync(__dirname + `/log.txt`, 'utf8');
-		const log_items = log.split(line);
-		log_items.shift();
+    const log = fs.readFileSync(__dirname + `/log.txt`, 'utf8');
+    const log_items = log.split(line);
+    log_items.shift();
 
-		const items = log_items.map(x => {
-			return x.trim().split('\n')
-		});
+    const items = log_items.map(x => {
+      return x.trim().split('\n')
+    });
 
-		const microblog_items = items.slice(-5).reverse();
+    const microblog_items = items.slice(-5).reverse();
 
-		console.log(microblog_items);
+    console.log(microblog_items);
 
-		const path = 'public/microblog.json';
-		fs.writeFileSync(path, JSON.stringify({
-			updated: new Date(),
-			items: microblog_items
-		}, null, 2))
+    const path = 'public/microblog.json';
+    fs.writeFileSync(path, JSON.stringify({
+      updated: new Date(),
+      items: microblog_items
+    }, null, 2))
 
     console.log('  ', ' 🦄 microblog complete, see public '.bgGreen.black);
 
